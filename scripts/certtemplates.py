38e721080e38748b17770ef6e97b50d3f9101380
Xenov <62152036+Xenov-X@users.noreply.github.com>
2022-09-13 16:58:08 +0100

    Update certtemplates.py

--- scripts/certtemplates.py
+++ scripts/certtemplates.py
@@ -29,6 +29,25 @@ for k,v in ades.dncache.items():
         aces = ades.parse_acl(None, 'user', ADUtils.get_entry_property(entry, 'nTSecurityDescriptor', raw=True))
         processed_aces = ades.resolve_aces(aces)
 
+        abc = entry.classes
+        msPKI_Certificate_Application_Policy = ADUtils.get_entry_property(entry, 'msPKI-Certificate-Application-Policy')
+        ADCS_List = [
+            'msPKI-Certificate-Application-Policy',
+            'msPKI-Certificate-Name-Flag',
+            'msPKI-Enrollment-Flag',
+            'flags',
+            'msPKI-Private-Key-Flag',
+            'msPKI-RA-Signature',
+            'pKIExtendedKeyUsage',
+            'showInAdvancedViewOnly'
+        ]
+        for attrib in ADCS_List:
+            attrib_prop = ADUtils.get_entry_property(entry, attrib)
+            if attrib_prop is not None:
+                if type(attrib_prop) == list:
+                    attrib_prop = '; '.join(attrib_prop)
+                print("{: >60} | {: <20}".format(attrib, attrib_prop))
+                
         for ace in processed_aces:
             pidx = ades.sidcache.get(ace['PrincipalSID'])
             if pidx:
